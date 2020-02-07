6c78006b4fec29c9b73629daf0b11ba206b40094
Mako Williams <57023467+makowilliams@users.noreply.github.com>
2020-02-06 20:59:45 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './state-drills/HelloWorld'
+
+function App() {
+  return (
+    <main className='App'>
+      <HelloWorld />
+    </main>
+  );
+}
+
+export default App;
